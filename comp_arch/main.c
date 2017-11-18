e526abc7395c99d2e9d2940c4537e6dd61391190
Andrews <c27t438@msu.montana.edu>
2017-11-17 19:26:41 -0700

    added to comp_arch project

--- comp_arch/main.c
+++ comp_arch/main.c
@@ -17,15 +17,13 @@ int main(void)
 	long long int sorted[15] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29};
 	long long int value_returned = binary_search(sorted, 15, 29);
 	printf("Result of binary search is %ld\n", value_returned);
-	/*  ******************************************
-	additional code for debugging. You can uncomment this code to test you implementation
+
 	assert( 1 == binary_search(sorted, 15, 1) );
 	assert( 3 == binary_search(sorted, 15, 3) );
 	assert( 15 == binary_search(sorted, 15, 15) );
 	assert( 23 == binary_search(sorted, 15, 23) );
 	assert( 29 == binary_search(sorted, 15, 29) );
 	assert( -1 == binary_search(sorted, 15, 35) );
-	 ****************************************** */
 
 
 	//code for part 3
