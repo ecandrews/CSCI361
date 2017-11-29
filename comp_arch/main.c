f87a04d25b10c26dcb7d8f04b9929232c7ea0f70
Andrews <c27t438@msu.montana.edu>
2017-11-28 19:18:39 -0700

    finished array, working on sort

--- comp_arch/main.c
+++ comp_arch/main.c
@@ -7,7 +7,7 @@
 
  // uncomment this code when you are ready to implement part three.
  // you may need to add parameters to match what is below
-extern long long int part_three();
+extern long long int* part_three(int num1, int num2, long long int unsorted[]);
 
 int main(void)
 {
@@ -16,7 +16,6 @@ int main(void)
 	//long long int sorted[15] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29};
 	//long long int value_returned = binary_search(sorted, 15, 29);
 	//printf("Result of binary search is %ld\n", value_returned);
-
 	//assert( 1 == binary_search(sorted, 15, 1) );
 	//assert( 3 == binary_search(sorted, 15, 3) );
 	//assert( 15 == binary_search(sorted, 15, 15) );
@@ -24,18 +23,18 @@ int main(void)
 	//assert( 29 == binary_search(sorted, 15, 29) );
 	//assert( -1 == binary_search(sorted, 15, 35) );
 
-
 	//code for part 3
 
 	// This code will call part 3. You may choose to add parameters,
 	// depending on the nature of your project
 
 	// generate the time
-	long long int unsorted[15] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	long long int aaronIsDumb[15] = part_three(2457257857, 15, unsorted);
-	for(int i = 0; i < 15; i++)
+	long long int unsorted[4] = {0, 0, 0, 0};
+	long long int* boib = part_three(24572, 4, unsorted);
+
+	for(int i = 0; i < 4; i++)
 	{
-	    printf("Result of part_three is %ld\n", aaronIsDumb[i]);
+	    printf("Result of part_three is %d\n", boib[i]);
 	}
 
     return 0;
